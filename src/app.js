020265663289a7248c53544a256712fe21056045
jaineel.upadhyay <jaineel.upadhyay@accenture.com>
2022-01-26 16:35:40 -0500

    File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -9,11 +9,42 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const accountData = fs.readFileSync("src/json/accounts.json", { encoding: 'UTF8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync("src/json/users.json", { encoding: "UTF8" });
+const users = JSON.parse(userData);
+
 app.get("/", function (req, res) {
     res.render("index", {
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
     });
 });
+
+app.get("/savings", function(req, res){
+    res.render("account", {
+        account:accounts.savings
+    });
+});
+
+app.get("/checking", function(req, res){
+    res.render("account", {
+        account:accounts.checking
+    });
+});
+
+app.get("/credit", function(req, res){
+    res.render("account", {
+        account:accounts.credit
+    });
+});
+
+app.get('/profile', function(req, res){
+    res.render("profile", {
+        user: users[0]
+    });
+})
 
 app.listen(3000, () => {
     console.log("PS Project Running on port 3000!");
